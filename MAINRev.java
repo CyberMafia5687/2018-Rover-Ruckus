d7f99f1d6969a19f20c992b97d71f6db9d80cc12
CyberMafia5687 <45892588+CyberMafia5687@users.noreply.github.com>
2019-01-07 10:40:50 -0800

    Update MAINRev.java
    Changed motors and servos. Preparing for sensors.

--- MAINRev.java
+++ MAINRev.java
@@ -1,80 +1,73 @@
 package org.firstinspires.ftc.teamcode;
 
+// Main Imports
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+// Extra Imports
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 
-// LAST UPDATED: 12/16/18 \\
+
+// LAST UPDATED: 1/7/19 \\
 @TeleOp
 // @Disabled
 public class MAINRev extends LinearOpMode {
-    private DcMotor frontLeftMotor;
-    private DcMotor frontRightMotor;
-    private DcMotor backLeftMotor;
-    private DcMotor backRightMotor;
+
+    // Motors
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
     private DcMotor liftMotor;
     private DcMotor armMotor;
+    private DcMotor sweepMotor;
 
-    public Servo handServo = null;
-    public CRServo sweepServo;
+    // Servos
+    private Servo markerServo;
+
+    // Sensors
+    DigitalChannel digitalMagLimit;
 
 
     @Override
     public void runOpMode() {
 
-        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
-        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
-        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
-        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        leftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
         liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
         armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+        sweepMotor = hardwareMap.get(DcMotor.class, "sweepMotor");
 
-        handServo = hardwareMap.get(Servo.class, "handServo");
-        sweepServo = hardwareMap.get(CRServo.class, "sweepServo");
+        markerServo = hardwareMap.get(Servo.class, "markerServo");
 
-        telemetry.addData("Status", "Running");
+        telemetry.addData("Status", "Initializing...");
         telemetry.update();
 
 
         // Wait for game to start (Driver, press PLAY)
         // waitForStart();
         while (!opModeIsActive() && !isStopRequested()){
-            telemetry.addData("Status", "Waiting for Start Command...");
+            telemetry.addData("Status", "Waiting for Start Command");
             telemetry.update();
         }
 
-        // Run until end of match (Driver, press STOP)
+        // Run until end of match (End of match, press STOP)
         double tgtPower1 = 0;
         double tgtPower2 = 0;
         double tgtPower3 = 0;
         double tgtPower4 = 0;
         double tgtPower5 = 0;
-        double tgtPower6 = 0;
 
         while (opModeIsActive()){
 
            // Tank Drive
            tgtPower1 = this.gamepad1.left_stick_y;
-           frontLeftMotor.setPower(tgtPower1);
-           backLeftMotor.setPower(tgtPower1);
+           leftMotor.setPower(tgtPower1);
 
            tgtPower2 = -this.gamepad1.right_stick_y;
-           frontRightMotor.setPower(tgtPower2);
-           backRightMotor.setPower(tgtPower2);
-
-           // Strafe Drive
-            /*
-           tgtPower3 = this.gamepad1.left_stick_x;
-           frontLeftMotor.setPower(tgtPower3);
-           backLeftMotor.setPower(-tgtPower3);
-
-           tgtPower4 = this.gamepad1.right_stick_x;
-           frontRightMotor.setPower(tgtPower4);
-           backRightMotor.setPower(-tgtPower4);
-           */
+           rightMotor.setPower(tgtPower2);
 
            // Moon Lander Lift Control
            if (this.gamepad1.dpad_up){
@@ -86,6 +79,7 @@ public class MAINRev extends LinearOpMode {
                sleep(3250);
                liftMotor.setPower(0);
            }
+
            /*
            else if (this.gamepad1.x){
                break;
@@ -109,31 +103,20 @@ public class MAINRev extends LinearOpMode {
            */
 
            // Precision Lift Controls
-           tgtPower5 = this.gamepad2.right_trigger;
-           liftMotor.setPower(tgtPower5);
-           tgtPower5 = this.gamepad2.left_trigger;
-           liftMotor.setPower(-tgtPower5);
+           tgtPower3 = this.gamepad2.right_trigger;
+           liftMotor.setPower(tgtPower3);
+           tgtPower3 = this.gamepad2.left_trigger;
+           liftMotor.setPower(-tgtPower3);
 
            // Arm Control
-           tgtPower6 = this.gamepad2.left_stick_y;
-           armMotor.setPower(-tgtPower6);
+           tgtPower4 = this.gamepad2.left_stick_y;
+           armMotor.setPower(-tgtPower4);
 
            // Sweeper Controls
            if(gamepad2.left_bumper){
-               sweepServo.setPower(10.0);
+               sweepMotor.setPower(tgtPower5);
            } else if(gamepad2.right_bumper){
-               sweepServo.setPower(-10.0);
-           } else{
-               sweepServo.setPower(0);
-           }
-
-           // Bucket Controls
-           if (gamepad2.y){
-               handServo.setPosition(1);
-           } else if (gamepad2.x){
-               handServo.setPosition(0.46);
-           } else{
-               handServo.setPosition(0);
+               sweepMotor.setPower(-tgtPower5);
            }
 
            }
@@ -142,5 +125,3 @@ public class MAINRev extends LinearOpMode {
            telemetry.update();
         }
     }
-
-// http://www.revrobotics.com/content/docs/REV-31-1153-GS.pdf
