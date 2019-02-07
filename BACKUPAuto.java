afcb2a845b862ecdc9e3f6f72db7410d0a9cf23b
CyberMafia5687 <45892588+CyberMafia5687@users.noreply.github.com>
2019-02-06 16:31:14 -0800

    MAINAuto is now BACKUPAuto
    Intended use:
    In case the gold mineral detection fails, the robot can still park in the crater.

--- BACKUPAuto.java
+++ BACKUPAuto.java
@@ -9,40 +9,51 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 // Extra Imports
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-// LAST UPDATED: 1/10/19 \\
+// LAST UPDATED: 2/1/19 \\
 @Autonomous
 // @Disabled
-public class MAINAuto extends LinearOpMode {
+public class BACKUPAuto extends LinearOpMode {
 
     // Motors
+    private DcMotor frontLeftMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backRightMotor;
     private DcMotor liftMotor = null;
 
     // Servos
-    private Servo markerServo;
+    // private Servo markerServo;
 
     // Sensors
     private BNO055IMU imu;
     // private DistanceSensor sensorRange;
     // DigitalChannel digitalMagLimit;
 
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-        telemetry.addData("Status", "Running");
-        telemetry.update();
+    private void robotInit(){
 
         // Motors
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        backRightMotor.setDirection(DcMotor.Direction.REVERSE);
+
         liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
         liftMotor.setDirection(DcMotor.Direction.FORWARD);
 
         // Servos
-        markerServo = hardwareMap.get(Servo.class, "markerServo");
+        // markerServo = hardwareMap.get(Servo.class, "markerServo");
 
         // Sensors
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -54,27 +65,44 @@ public class MAINAuto extends LinearOpMode {
 
         // digitalMagLimit = hardwareMap.get(DigitalChannel.class, "sensor_mag_limit");
         // digitalMagLimit.setMode(DigitalChannel.Mode.INPUT);
+    }
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initializing the HitBot");
+        telemetry.update();
+
+        robotInit();
 
         //waitForStart();
         while (!opModeIsActive() && !isStopRequested()){
-            telemetry.addData("Status", "Waiting for Start Command...");
+            telemetry.addData("Status", "HitBot is waiting for its next mission");
             telemetry.update();
         }
 
         // Landing v1 w/ Adjustments
         if (opModeIsActive()) {
             liftMotor.setPower(-0.65);
-            sleep(3500);
+            sleep(4550);
             liftMotor.setPower(0);
         }
 
-        /*
-        // Landing v2 (BETA)
-        if (digitalMagLimit.getState()){
-            liftMotor.setPower(-0.4);
-        } else{
-            liftMotor.setPower(0);
-        }
-        */
+        sleep(1500);
+
+        frontLeftMotor.setPower(-0.5);
+        backLeftMotor.setPower(-0.5);
+        frontRightMotor.setPower(-0.5);
+        backRightMotor.setPower(-0.5);
+        sleep(2500);
+        frontLeftMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backRightMotor.setPower(0);
+
+        liftMotor.setPower(0.65);
+        sleep(4550);
+        liftMotor.setPower(0);
+
     }
 }
