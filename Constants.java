ddabc96af514534ab268a5910a6443a037dccdd6
CyberMafia5687 <45892588+CyberMafia5687@users.noreply.github.com>
2019-03-24 14:06:38 -0700

    New constants

--- Constants.java
+++ Constants.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-// LAST UPDATED: 12/15/18 \\
+// LAST UPDATED: 1/20/19 \\
 public class Constants {
     // Put Constants here for use in other codes
-    static final double DRIVE_ARMMOTOR_COUNTS_PER_DEGREE = (40 * (72 / 45) * (90 / 30) * (125 / 30) * 56) / 360;
+    static final double DRIVE_ARMMOTOR_COUNTS_PER_DEGREE = (40.0 * (72.0 / 45) * (90.0 / 30) * (125.0 / 30) * 56) / 360;
+    // New Constant
+    static final double DRIVE_DRIVEMOTOR_COUNTS_PER_DEGREE = 280.0 / 360;
+    static final double HITBOT_DRIVEMOTOR_COUNTS_DEGREE = 0.9 / 360;
 }
