b5ddb4bb31464b302f2103e535f91a6f40f4e60c
CyberMafia5687 <45892588+CyberMafia5687@users.noreply.github.com>
2019-03-24 14:38:27 -0700

    Update MAINRev.java

--- MAINRev.java
+++ MAINRev.java
@@ -4,15 +4,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 // Extra Imports
-import com.qualcomm.robotcore.hardware.DigitalChannel;
+// import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 
-// LAST UPDATED: 1/14/19 \\
+// LAST UPDATED: 1/30/19 \\
 @TeleOp
 // @Disabled
 public class MAINRev extends LinearOpMode {
@@ -23,14 +22,16 @@ public class MAINRev extends LinearOpMode {
     private DcMotor frontRightMotor;
     private DcMotor backRightMotor;
     private DcMotor liftMotor;
-    private DcMotor armMotor1;
-    private DcMotor armMotor2;
+    private DcMotor armMotor;
     private DcMotor sweepMotor;
 
     // Servos
 
     // Sensors
-    //DigitalChannel digitalMagLimit;
+    // DigitalChannel digitalMagLimit;
+
+    // Encoder Command
+    //
 
 
     @Override
@@ -40,9 +41,14 @@ public class MAINRev extends LinearOpMode {
         backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
         backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+
         liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
-        armMotor1 = hardwareMap.get(DcMotor.class, "armMotor1");
-        armMotor2 = hardwareMap.get(DcMotor.class, "armMotor2");
+        /*
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        */
+
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
         sweepMotor = hardwareMap.get(DcMotor.class, "sweepMotor");
 
         /*
@@ -50,18 +56,13 @@ public class MAINRev extends LinearOpMode {
         digitalMagLimit.setMode(DigitalChannel.Mode.INPUT);
         */
 
-        telemetry.addData("Status", "Initializing...");
-        telemetry.update();
-
 
         // Wait for game to start (Driver, press PLAY)
-        waitForStart();
-        /*
+        // waitForStart();
         while (!opModeIsActive() && !isStopRequested()){
-            telemetry.addData("Status", "Waiting for Start Command");
+            telemetry.addData("Status", "Waiting for  next mission...");
             telemetry.update();
         }
-        */
 
         // Run until end of match (End of match, press STOP)
         double tgtPower1 = 0;
@@ -72,6 +73,9 @@ public class MAINRev extends LinearOpMode {
 
         while (opModeIsActive()){
 
+           telemetry.addData("Status", "HitBot is Running");
+           telemetry.update();
+
            // Tank Drive
            tgtPower1 = this.gamepad1.left_stick_y;
            frontLeftMotor.setPower(tgtPower1);
@@ -80,6 +84,15 @@ public class MAINRev extends LinearOpMode {
            tgtPower2 = -this.gamepad1.right_stick_y;
            frontRightMotor.setPower(tgtPower2);
            backRightMotor.setPower(tgtPower2);
+
+           // Tank Drive, Controlled v1
+           if(this.gamepad1.right_bumper){
+               frontLeftMotor.setPower(tgtPower1 / 8);
+               backLeftMotor.setPower(tgtPower1 / 8);
+               frontRightMotor.setPower(tgtPower2 / 8);
+               backRightMotor.setPower(tgtPower2 / 8);
+           }
+
 
            /*
            // Moon Lander Lift Control v1
@@ -122,24 +135,64 @@ public class MAINRev extends LinearOpMode {
 
             */
 
-           // Precision Lift Controls
+           /*
+           // Moon Lander Lift Control v4 (BETA)
+           while (digitalMagLimit.getState()){
+               if (this.gamepad1.dpad_up){
+                   liftMotor.setPower(0.5);
+               } else if(this.gamepad1.dpad_down){
+                   liftMotor.setPower(0.5);
+               } else{
+                   liftMotor.setPower(0);
+               }
+           }
+           */
+
+           // TODO New Lift Mechanism
+           // Moon Lander Lift Control v5 (with Jeremy)
+            /*
+             if (this.gamepad1.y){
+                liftMotor.setPower(-0.4);
+                sleep(500);
+                liftMotor.setPower(0);
+             } else if (this.gamepad1.x){
+                liftMotor.setPower(0.4);
+                sleep(500);
+                liftMotor.setPower(0);
+
+             if (this.gamepad1.dpad_up && !digitalMagLimit.getState() && encoderCount > x){
+               liftMotor.setPower(-0.5);
+           } else if (this.gamepad1.dpad_down && !digitalMagLimit.getState() && encoderCount < x){
+               liftMotor.setPower(0.5);
+           } else{
+                liftMotor.setPower(0);
+           }
+           */
+
+
+           // Manual Lift Controls
            tgtPower3 = this.gamepad2.right_trigger;
            liftMotor.setPower(tgtPower3);
            tgtPower3 = this.gamepad2.left_trigger;
            liftMotor.setPower(-tgtPower3);
 
-           // Arm Control
-           tgtPower4 = this.gamepad2.left_stick_y;
-           armMotor1.setPower(-tgtPower4);
-           armMotor2.setPower(-tgtPower4);
 
-           // Sweeper Controls
-           tgtPower5 = this.gamepad2.right_stick_y;
+           // Arm Control
+           tgtPower4 = -this.gamepad2.left_stick_y;
+           armMotor.setPower(tgtPower4);
+
+
+           // Sweeper Control
+           tgtPower5 = -this.gamepad2.right_stick_y;
            sweepMotor.setPower(tgtPower5);
 
+
+           // Sweeper and Arm Controls, Controlled
+           if(this.gamepad2.left_bumper){
+               sweepMotor.setPower(tgtPower5 / 8);
+               armMotor.setPower(tgtPower4 / 2);
            }
 
-           telemetry.addData("Status", "Running");
-           telemetry.update();
-        }
-    }
+          }
+       }
+   }
